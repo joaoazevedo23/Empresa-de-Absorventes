981b6223b8d67527b6ad8b75a53e7e2934be1b82
João Paulo Azevedo <azevedo.jpf@gmail.com>
2024-12-13 00:15:02 -0300

    Atualização da Home e dos Styles

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -5,7 +5,12 @@ function Home(){
     return(
         <div class="caixa">
             <header class="header">
-                <h1>Empresa de Absorventes</h1>
+                <h1 class="titulo">Empresa de Absorventes</h1>
+                <div class="elements">
+                    <h3 class="pages">Home</h3>
+                    <h3 class="pages">Sobre Nós</h3>
+                    <h3 class="pages">Entre em Contato</h3>
+                </div>
             </header>
         </div>
     )
