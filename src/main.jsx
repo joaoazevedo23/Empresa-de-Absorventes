985c34b978f6465e7e57da6b7e53115913cdaabf
João Paulo Azevedo <azevedo.jpf@gmail.com>
2024-12-12 22:58:28 -0300

    Criação da Home e do Register

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import  Home  from './pages/Home/home'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <App /> */}
+     <Home /> 
   </StrictMode>,
 )
---
+++ src/pages/Home/home.jsx
@@ -0,0 +1,14 @@
+import './styles.scss'
+
+
+function Home(){
+    return(
+        <div class="caixa">
+            <header class="header">
+                <h1>Empresa de Absorventes</h1>
+            </header>
+        </div>
+    )
+}
+
+export default Home
